82d0753777e031be2668bfbff4f3e919e68f7054
Andrei O <andrei@flashsoft.eu>
2024-07-09 15:29:37 +0300

    chore: add types

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,32 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
+import typescript from '@rollup/plugin-typescript'
 
 declare const __dirname: string;
+
+
+const resolvePath = (str: string) => path.resolve(__dirname, str)
+
 
 export default defineConfig({
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, 'src'),
+			'@': resolvePath('src'),
 		},
 	},
 	plugins: [
+		typescript({'target': 'es2020',
+			'rootDir': resolvePath('src'),
+			'declaration': true,
+			'declarationDir': resolvePath('dist'),
+			exclude: resolvePath('node_modules/**'),
+			allowSyntheticDefaultImports: true,
+			sourceMap: false,
+}),
 	],
 	build: {
 		lib: {
-			entry: path.resolve(__dirname, 'src/main.ts'),
+			entry: resolvePath('src/main.ts'),
 			name: 'YupAPI',
 			fileName: (format) => `yup-api-interact.${format}.js`,
 		},
